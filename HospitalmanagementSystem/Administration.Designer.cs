7dcf6bdf1d96529826ac59d55f9528f7681bca26
ritika <ritika@DESKTOP-TML5DJH>
2022-01-09 13:28:10 +1000

    Changes

--- HospitalmanagementSystem/Administration.Designer.cs
+++ HospitalmanagementSystem/Administration.Designer.cs
@@ -22,10 +22,7 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+        
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
